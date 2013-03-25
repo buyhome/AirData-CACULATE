9a872ee75f30ed92ecc79d6ef514e471dc44925b
buyhome <pai6@qq.com>
2013-03-25 13:17:31 +0800

    Add book
    http://stomp.fusesource.org/documentation/php/book.html

--- faredb/rfare.php
+++ faredb/rfare.php
@@ -3,7 +3,7 @@
  *
  * Copyright (C) 2009 Progress Software, Inc. All rights reserved.
  * http://fusesource.com
- *
+ * http://stomp.fusesource.org/documentation/php/book.html
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
